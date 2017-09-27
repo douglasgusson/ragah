7367a5f4f38955bdd73d2b457bbbc047ac03ed67
Douglas Gusson <douglasgusson@gmail.com>
2017-09-27 13:59:15 -0300

    adicionada anotacoes do lombok

--- src/br/com/ragah/domain/EstadoCivil.java
+++ src/br/com/ragah/domain/EstadoCivil.java
@@ -1,11 +1,17 @@
 package br.com.ragah.domain;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  *
  * @author Douglas Gusson
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EstadoCivil {
 
     private Long id;
--- src/br/com/ragah/domain/Funcionario.java
+++ src/br/com/ragah/domain/Funcionario.java
@@ -3,11 +3,17 @@ package br.com.ragah.domain;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  *
  * @author Douglas Gusson
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Funcionario {
 
     private int matricula;
@@ -28,22 +34,4 @@ public class Funcionario {
     private LocalDateTime criacao;
     private LocalDateTime alteracao;
 
-    /*
-        matricula        INTEGER      NOT NULL,
-        nome             VARCHAR(50)  NOT NULL, 
-        cpf              CHAR(11)     NOT NULL,
-        rg               VARCHAR(20)  NOT NULL,
-        data_nascimento  DATE         NOT NULL,
-        estado_civil     INTEGER      NOT NULL,
-        salario          NUMERIC(7,2) NOT NULL,
-        data_admissao    DATE         NULL,
-        data_demissao    DATE         NOT NULL,
-        funcao           INTEGER      NOT NULL,
-        endereco         VARCHAR(60)  NOT NULL,
-        cep              CHAR(8)      NOT NULL,  
-        bairro           VARCHAR(30)  NOT NULL,
-        cidade           INTEGER      NOT NULL,
-        data_criacao     TIMESTAMP    NOT NULL,
-        data_atualizacao TIMESTAMP    NOT NULL,
-     */
 }
